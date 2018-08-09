eca6dc406b90d5550b628c37c546c059c085d2fd
alvalle09 <phredvalle@gmail.com>
2018-08-08 23:45:33 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>geogram</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<form action="" method="get">
+    <input type="text" name="location"/>
+    <button type="submit">Submit</button>
+</form>
+<br/>
+<div id="results" >
+    <?php
+    if () {
+        
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
